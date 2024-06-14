efc57de1f322618b369a98725309aac824df6264
elizabeth-agada <elizadave6@gmail.com>
2024-06-14 18:09:21 +0100

    update

--- src/pages/ExpertsPage.jsx
+++ src/pages/ExpertsPage.jsx
@@ -57,7 +57,7 @@ const ExpertsPage = () => {
                 className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-200"
                 onClick={() => handleViewDetails(expert)}
               >
-                {contacted ? 'Submit Review' : 'Contact Expert'}
+                {contacted ? 'Submit Review' : 'View Expert'}
               </button>
             </div>
           ))}
