a34da848ec0ef605fbb806afea1b83d1f28b7fb2
leoklems <leoklems00@gmail.com>
2024-06-19 05:54:29 +0100

    form correction

--- src/api.js
+++ src/api.js
@@ -4,7 +4,8 @@ import { ACCESS_TOKEN } from "./constants";
 const apiUrl = "https://mysite-vtk8.onrender.com";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  // baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: apiUrl,
 });
 
 api.interceptors.request.use(
