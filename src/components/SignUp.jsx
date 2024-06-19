60e076c77c4a0646ddb55de23cb751028b0323a5
leoklems <leoklems00@gmail.com>
2024-06-19 06:19:32 +0100

    form correction

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -5,15 +5,35 @@ import { Link } from 'react-router-dom'; // If using React Router for navigation
 // import { useNavigate } from "react-router-dom";
 // import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 // import "../styles/Form.css"
-// import LoadingIndicator from "./LoadingIndicator";
+import LoadingIndicator from "./LoadingIndicator";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [loading, setLoading] = useState(false);
-  // const navigate = useNavigate();
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
+    // Handle sign-in logic here
+    try {
+
+      // const res = await api.post(route, { username, password })
+      const res = await api.post("/patient/register/", { name, email, password })
+
+      // if (method === "login") {
+          // localStorage.setItem(ACCESS_TOKEN, res.data.access);
+          // localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+          navigate("/")
+      // } else {
+      //     navigate("/login")
+      // }
+    } catch (error) {
+        alert(error)
+    } finally {
+        setLoading(false)
+    }
     // Handle sign-up logic here
   };
 
@@ -31,6 +51,9 @@ const SignUp = () => {
               type="text"
               id="name"
               name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Full Name"
               required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
@@ -65,7 +88,7 @@ const SignUp = () => {
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
-          {/* {loading && <LoadingIndicator />} */}
+          {loading && <LoadingIndicator />}
           <button
             type="submit"
             className="w-full bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
