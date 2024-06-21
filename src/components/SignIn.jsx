0343dbbdbd8172d904660744a0a517a2855102a4
leoklems <leoklems00@gmail.com>
2024-06-21 16:48:24 +0100

    resolving anonymous user

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -38,8 +38,8 @@ const SignIn = () => {
         // setUserData(userData);
         // const res = await api.post(route, { username, password })
         console.log(userData)
-        const res = api.post("/api/token/", {username, password}).
-        then(response =>  {
+        const res = api.post("/api/token/", {username, password})
+        .then(response =>  {
           const data = response.data;
           console.log(data)
         });
